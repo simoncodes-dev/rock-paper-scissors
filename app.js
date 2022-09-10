817a2456a5858a7f9eb788e1afc235d48441ba60
Simon F <105530034+simoncodes-dev@users.noreply.github.com>
2022-09-09 17:32:57 -0700

    Add Reset Functionality

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const playerScoreText = document.getElementById('player-score-text')
 const computerScoreText = document.getElementById('computer-score-text')
 const winnerText = document.getElementById('winner-text')
 
-const resetButton = document.getElementById('reset-button')
+const resetBtn = document.getElementById('reset-button')
 
 const runRound = (playerChoice) => {
     const computerChoice = choiceArray[Math.floor(Math.random() * 3)]
@@ -46,13 +46,25 @@ const gameOver = () => {
         alert(`Game Over! ${playerWins === 5 ? 'Player wins!' : 'Computer wins!'}`)
         winnerText.innerHTML = `Game Over! ${playerWins === 5 ? 'Player wins!' : 'Computer wins!'}`
         winnerText.style.display = 'block'
-        resetButton.classList.remove('d-none')
+        resetBtn.classList.remove('d-none')
         return true;
     }
     return false;
 }
 
+const resetGame = () => {
+    computerScoreText.innerHTML = '0'
+    playerScoreText.innerHTML = '0'
+    gameOverFlag = false
+    winnerText.innerHTML = ''
+    winnerText.style.display = 'none'
+    resetBtn.classList.add('d-none')
+    playerWins = 0
+    computerWins = 0
+    round = 0
+}
 
 rockBtn.addEventListener('click', () => runRound('rock'))
 paperBtn.addEventListener('click', () => runRound('paper'))
 scissorsBtn.addEventListener('click', () => runRound('scissors'))
+resetBtn.addEventListener('click', resetGame)
